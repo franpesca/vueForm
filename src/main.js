4372d89d2a7fb1bc45e0e4f4d9b93955d3035e46
Francesca Citter <francesca.citter@link-me.it>
2019-05-24 12:44:34 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { store } from './store/store.js';
+
+Vue.use(Vuex)
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
